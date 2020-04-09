dd631d968b0179917876ec248b6fa63689a7c395
LisonFan <lisonfan1996@gmail.com>
2020-04-09 10:18:45 +0800

    first commit

---
+++ WXChinaWeatherSDKDemo/WXChinaWeatherSDKDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WXChinaWeatherSDKDemo
+//
+//  Created by LisonFan on 2020/4/9.
+//  Copyright © 2020 voc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
